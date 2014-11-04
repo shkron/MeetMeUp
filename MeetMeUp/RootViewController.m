b244ba06de0b01979a99e676974a311212bcf4ff
Alexey Emelyanov <emelyanoff@gmail.com>
2014-11-03 20:05:53 -0800

    webVIew, search implemented

--- MeetMeUp/RootViewController.m
+++ MeetMeUp/RootViewController.m
@@ -10,10 +10,11 @@
 #import "DetailViewController.h"
 #define kStartingPoint @"https://api.meetup.com/2/open_events.json?zip=94103&text=mobile&time=,1w&key=3c7f626d333e3a7433a44552f6b775f"
 
-@interface RootViewController () <UITableViewDelegate, UITableViewDataSource>
+@interface RootViewController () <UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate>
 @property (nonatomic, weak) IBOutlet UITableView *tableView;
 @property (nonatomic, strong) NSArray *tableViewArray;
 @property (nonatomic, strong) NSArray *dataMethodArray;
+@property (weak, nonatomic) IBOutlet UITextField *searchTextField;
 
 @end
 
@@ -24,25 +25,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-//    [self dataWithURLRequest:kStartingPoint];
+    [self dataWithURLString:kStartingPoint];
 //    self.tableViewArray = self.dataMethodArray;
 
-    NSURL *url = [NSURL URLWithString:kStartingPoint];
-    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-    [NSURLConnection sendAsynchronousRequest:request
-                                       queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
-                                           if (connectionError)
-                                           {
-                                               [self netowrkAlertWindow:connectionError.localizedDescription];
-                                           }
-                                           else
-                                           {
-                                               NSDictionary *rawDict = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-                                               self.tableViewArray = [rawDict objectForKey:@"results"];
-                                               [self.tableView reloadData];
-                                           }
-                                       }];
-
+//    NSURL *url = [NSURL URLWithString:kStartingPoint];
+//    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+//    [NSURLConnection sendAsynchronousRequest:request
+//                                       queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+//                                           if (connectionError)
+//                                           {
+//                                               [self netowrkAlertWindow:connectionError.localizedDescription];
+//                                           }
+//                                           else
+//                                           {
+//                                               NSDictionary *rawDict = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+//                                               self.tableViewArray = [rawDict objectForKey:@"results"];
+//                                               [self.tableView reloadData];
+//                                           }
+//                                       }];
+//
 
 
 
@@ -107,7 +108,7 @@ return self.tableViewArray.count;
 
 #pragma mark - methods
 
--(void)netowrkAlertWindow:(NSString *)message
+-(void)networkAlertWindow:(NSString *)message
 {
     UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Network Error" message:message preferredStyle:UIAlertControllerStyleAlert];
     UIAlertAction *okButton = [UIAlertAction actionWithTitle:@"Gosh Darnet" style:UIAlertActionStyleDefault handler:nil];
@@ -115,8 +116,44 @@ return self.tableViewArray.count;
     [self presentViewController:alert animated:YES completion:nil];
 }
 
-//-(void)dataWithURLRequest:(NSString *)urlString
-//{
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+
+    if (![self.searchTextField.text isEqualToString:@""])
+    {
+        NSString *searchString = [NSString stringWithFormat:@"https://api.meetup.com/2/open_events.json?zip=94103&text=%@&time=,1w&key=3c7f626d333e3a7433a44552f6b775f", self.searchTextField.text];
+        [self dataWithURLString:searchString];
+        [self.searchTextField resignFirstResponder];
+
+    }
+    else
+    {
+
+    }
+
+    return YES;
+}
+
+
+-(void)dataWithURLString:(NSString *)urlString
+{
+
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [NSURLConnection sendAsynchronousRequest:request
+                                       queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+                                           if (connectionError)
+                                           {
+                                               [self networkAlertWindow:connectionError.localizedDescription];
+                                           }
+                                           else
+                                           {
+                                               NSDictionary *rawDict = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+                                               self.tableViewArray = [rawDict objectForKey:@"results"];
+                                               [self.tableView reloadData];
+                                           }
+                                       }];
+
 
 //    NSURL *url = [NSURL URLWithString:@"https://api.meetup.com/2/open_events.json?zip=94103&text=mobile&time=,1w&key=3c7f626d333e3a7433a44552f6b775f"];
 //    NSURL *url = [NSURL URLWithString:kStartingPoint];
@@ -148,7 +185,7 @@ return self.tableViewArray.count;
 //        });
 //        [data release];
 //    });
-//}
+}
 
 #pragma mark - segue
 
--- MeetMeUp/WebViewController.m
+++ MeetMeUp/WebViewController.m
@@ -8,19 +8,57 @@
 
 #import "WebViewController.h"
 
-@interface WebViewController ()
+@interface WebViewController () <UIWebViewDelegate>
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
 
 @end
 
 @implementation WebViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-   [self.webView 
+    [self.webView loadRequest:self.urlRequest];
 }
 
 
+-(void)webViewDidStartLoad:(UIWebView *)webView
+{
+    [self.activityIndicator startAnimating];
 
+
+}
+
+-(void)webViewDidFinishLoad:(UIWebView *)webView
+{
+    [self.activityIndicator stopAnimating];
+    NSString *webTitle = ([self.webView stringByEvaluatingJavaScriptFromString:@"document.title"]);
+    self.navigationItem.title = webTitle;
+}
+
+
+- (IBAction)goBackButton:(UIButton *)sender
+{
+
+    if(self.webView.canGoBack)
+    {
+        [self.webView goBack];
+    }
+    else{}
+
+}
+
+
+- (IBAction)goForwardButton:(UIButton *)sender
+{
+
+    if(self.webView.canGoForward)
+    {
+        [self.webView goForward];
+    }
+    else{}
+
+}
 
 @end
